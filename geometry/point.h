10007aa75cff784a0042780255b84a702ebac7d7
Jishan Shaikh <30091032+Jishanshaikh4@users.noreply.github.com>
2018-07-29 12:04:59 +0530

    Create point.h

---
+++ geometry/point.h
@@ -0,0 +1,17 @@
+#include <math.h>
+
+void create2DPointFloat(float x, float y){
+
+}
+
+void create2DPointInt(int x, int y){
+
+}
+
+void create3DPointFloat(float x, float y, float z){
+
+}
+
+void create3DPointInt(int x, int y, int z){
+
+}
