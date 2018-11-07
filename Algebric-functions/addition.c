6e7cc944452d868ee5b4a54649c3923f95074bf2
Jishan Shaikh <30091032+Jishanshaikh4@users.noreply.github.com>
2018-11-07 22:03:58 +0530

    Create addition.c

---
+++ Algebric-functions/addition.c
@@ -0,0 +1,10 @@
+/*  ADDITION PROGRAM
+  This program is not simple as it seems, it should be compatible with all data types, and
+  there shouldn't be any problem of buffer overflows.
+*/
+
+#include <stdio.h>
+
+int main(){
+  return 0;
+}
