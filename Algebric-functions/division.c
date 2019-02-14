6b0776ee4af03d3c03f4c3e46136b56d3b5cb9ef
Jishan Shaikh <30091032+Jishanshaikh4@users.noreply.github.com>
2019-02-14 21:41:23 +0530

    Create division.c

---
+++ Algebric-functions/division.c
@@ -0,0 +1,10 @@
+/*  PROGRAM
+  This program is not simple as it seems, it should be compatible with all data types, and
+  there shouldn't be any problem of buffer overflows.
+*/
+
+#include <stdio.h>
+
+__int128 main(){
+  return 0;
+}
