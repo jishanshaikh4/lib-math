8b40ce835fa711f07b2ca5b00987c33b7b7b14a3
Jishan Shaikh <30091032+Jishanshaikh4@users.noreply.github.com>
2019-02-15 15:28:36 +0530

    Created Euler-method.py
    Code ported from C to python3.

---
+++ numerical-methods/Python3/euler-method.py
@@ -0,0 +1,20 @@
+# Euler Method
+
+import os
+import sys
+
+def df(a, b):
+  return a+b
+  
+if __name__ == "__name__":
+  x0, y0, h, x = input.split()
+  x1 = x0
+  y1 = y0
+  while 1:
+    if x1 > x:
+      break
+    y1 += h * df(x1, y1);
+    x1 += h
+    # print("When x = %3.1f, y = %4.2f\n", x1, y1);
+    
+# End of program euler-method.py
