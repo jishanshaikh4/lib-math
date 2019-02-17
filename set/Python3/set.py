eedec26d375552dfe983cf91daf4b78c08484cbe
Jishan Shaikh <30091032+Jishanshaikh4@users.noreply.github.com>
2019-02-17 22:05:41 +0530

    Create set.py

---
+++ set/Python3/set.py
@@ -0,0 +1,20 @@
+# Python implementation of set.c and set.h
+# Project: lib-math; portable to Python 3.x
+# TO DO: Improve naming conventions
+
+import os
+import sys
+
+class set:
+  int n;
+  int[n] arr;
+  # Add more information?
+  def push_back(s, a)
+  def delete_element(s, b)
+  def size_of_set(s)
+  def max_element_of_set(s)
+  def min_element_of_set(s)
+  
+if __main__ == "__main__":
+  # Utility code here.
+
