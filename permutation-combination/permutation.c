5f5a21b0dfe84c388df21cd4865b0570ef00e928
Jishan Shaikh <30091032+Jishanshaikh4@users.noreply.github.com>
2019-02-17 22:09:32 +0530

    Update permutation.c

--- permutation-combination/permutation.c
+++ permutation-combination/permutation.c
@@ -1,1 +1,22 @@
+/*
+  FILENAME: permutation.c
+  Project: lib-math
+*/
+
+// Main task is to generate all possible permutations of a given sequence
+
+#include <stdio.h>
+#include "string.h"
+
+#DEFINE SUCCESS 0;
+
+// fstring declared in string.h 
+void next_permutation(fstring input_string){
+  # Main code here
+}
+
+__int128 main(){
+  // Driver code here
+  return SUCCESS;
+}
 
