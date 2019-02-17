0457fc467269cb854d7889ee1095fc078fec2fd1
Jishan Shaikh <30091032+Jishanshaikh4@users.noreply.github.com>
2019-02-17 21:57:34 +0530

    Create set.c

---
+++ set/set.c
@@ -0,0 +1,16 @@
+/*
+  FILENAME: set.c
+  IMPLEMENTATION OF set.h
+  Project: lib-math
+*/
+
+#include <stdio.h>
+
+__int128 main(){
+  int n;
+  scanf("%d", &n);
+  int arr[n];
+  // USEFUL CODE HERE
+  
+  return SUCCESS;
+}
