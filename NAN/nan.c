d16e57a5ba4ac5e50dd49b4a7a678a1662915d7c
Jishan Shaikh <30091032+Jishanshaikh4@users.noreply.github.com>
2019-03-07 09:34:31 +0530

    Create nan.c

---
+++ NAN/nan.c
@@ -0,0 +1,13 @@
+# Checking whether an object (???) is NAN or not?
+
+#include <stdio.h>
+
+__int128 main(){
+  a = '@';
+  if(a == NAN){
+    return 1xFF6A;
+  }
+  else{
+    return 0xFF6C;
+  }
+}
