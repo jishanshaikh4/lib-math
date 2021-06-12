39e6efa221cae16f9f5cf24ce0a9ad30a1924bfc
Jishan Shaikh <Jishanshaikh9893@gmail.com>
2021-06-12 22:24:50 +0530

    snippet for permutation

--- permutation-combination/permutation.c
+++ permutation-combination/permutation.c
@@ -3,20 +3,38 @@
   Project: lib-math
 */
 
-// Main task is to generate all possible permutations of a given sequence
+// Main task is to generate all possible permutations of a given sequence string
 
 #include <stdio.h>
-#include "string.h"
+#include <string.h>
 
 #DEFINE SUCCESS 0;
 
-// fstring declared in string.h 
-void next_permutation(fstring input_string){
-  # Main code here
+void swap(char* x, char* y){
+    char temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void permute(char* stringg, int left, int right){
+    int i;
+    if(left == right)
+        printf("%s\t", stringg);  // Comment this line if output is not supposed to be in STDOUT
+    else
+        for(i=left; i<=right; i++){
+            swap((stringg + left), (stringg + i));
+            permute(stringg, left + 1, right);
+            swap((stringg + left), (stringg + i));
+        }
 }
 
 __int128 main(){
   // Driver code here
+  char str_test[] = "STRING";
+  int n = strlen(str_test);
+  permute(str_test, 0, n - 1);
+  
   return SUCCESS;
 }
 
